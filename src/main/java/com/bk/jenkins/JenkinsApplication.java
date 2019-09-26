72fc5e4551c3aa3ae3808e24c6f104fd9c97d34d
石江华 <shijianghua@bakclass.com>
2019-09-26 15:44:34 +0800

    war包

--- src/main/java/com/bk/jenkins/JenkinsApplication.java
+++ src/main/java/com/bk/jenkins/JenkinsApplication.java
@@ -2,9 +2,13 @@ package com.bk.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-public class JenkinsApplication {
+@EnableCaching
+public class JenkinsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsApplication.class, args);
