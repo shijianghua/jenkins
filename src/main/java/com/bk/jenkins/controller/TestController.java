9493b7c988536b18bb51cad19dc2d297ad8629e5
石江华 <shijianghua@bakclass.com>
2019-09-26 14:35:51 +0800

    test

---
+++ src/main/java/com/bk/jenkins/controller/TestController.java
@@ -0,0 +1,28 @@
+/**
+ * @Title: TestController.java
+ * @Package: com.bk.jenkins.controller
+ * Company: 湖南教育出版社-贝壳网
+ * Copyright (c) 2019, www.bakclass.com. All Rights Reserved.
+ */
+package com.bk.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试
+ *
+ * @author shijh
+ * @date 2019/9/26 14:24
+ */
+@RestController
+public class TestController {
+
+
+    @GetMapping("test")
+    public String test(@RequestParam("name") String name){
+        return "你好：" + name;
+    }
+
+}
